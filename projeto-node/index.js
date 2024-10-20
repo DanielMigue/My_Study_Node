48d7975dc351480e0fe30173adc51df87ab75b6e
Daniel Miguel <117693873+DanielMigue@users.noreply.github.com>
2024-10-20 06:52:17 -0300

    Globais e módulos
    Trablhando com criação de pacote, exportação de dados e required

---
+++ projeto-node/index.js
@@ -0,0 +1,4 @@
+const calc = require('./modules/calculadora')
+
+console.log(calc.soma(5,4))
+console.log(calc.subtrair(5,4))
---
+++ projeto-node/modules/calculadora.js
@@ -0,0 +1,10 @@
+const calculadora = {
+    soma: function(a,b){
+        return a+b;
+    },
+    subtrair: function(a,b){
+        return a-b;
+    },
+}
+
+module.exports = calculadora /*exportando dados */
